bd82d504b2c6de4a7131bbce4b5f832763af5f3d
alieppyuu12 <alieppxrpl3@gmail.com>
2025-12-29 19:04:54 +0700

    fix: header

--- src/components/home/HomeHeader.tsx
+++ src/components/home/HomeHeader.tsx
@@ -2,7 +2,7 @@ export function HomeHeader() {
   return (
     <header className="home-header">
       <h1>Welcome</h1>
-      <p>Your calm personal workspace</p>
+      <p>A calm personal workspace</p>
     </header>
   )
 }
