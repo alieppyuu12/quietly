3c3ce0e5e58dc65dd3d60a7d1f94592c7536c7bb
alieppyuu12 <alieppxrpl3@gmail.com>
2026-01-02 04:58:37 +0700

    add CTA

---
+++ src/components/marketing/CTA.tsx
@@ -0,0 +1,8 @@
+export function CTA() { 
+    return (
+        <section className="cta"> 
+            <button>Get started</button>
+            <button>Request a demo</button>
+        </section>
+    )
+}
