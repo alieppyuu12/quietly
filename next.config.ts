2f21ca6e68e9ed6ff910e741095cd5dfdcc7749c
alieppyuu12 <alieppxrpl3@gmail.com>
2026-02-04 19:10:06 +0700

    config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  // other properties
 };
-
-export default nextConfig;
