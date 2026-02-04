c92207b5930341c8b6c65a696293c101036f833b
alieppyuu12 <alieppxrpl3@gmail.com>
2026-02-04 19:14:26 +0700

    src/components/marketing/Navbar.tsx

--- src/components/marketing/Navbar.tsx
+++ src/components/marketing/Navbar.tsx
@@ -4,11 +4,7 @@ export function Navbar() {
   return (
     <nav className="nav">
       <div className="nav-left">Quietly</div>
-      <div className="nav-center">
-        <span>Product</span>
-        <span>How it works</span>
-        <span>Request a demo</span>
-      </div>
+
       <div className="nav-right">
         <Link href="/sign-in">
           <button>Log in</button>
